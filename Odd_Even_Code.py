6458f0e1ed498e237f11a65172972cd85f424122
Nabia Muskaan <mukthadir28@gmail.com>
2025-06-27 18:50:49 +0530

    I made a simple even or odd program using python

---
+++ Odd_Even_Code.py
@@ -0,0 +1,8 @@
+
+    # A simple python program for printing weather the given number is even or odd 
+
+a = 4
+if(a%2==0):
+    print("The given number is Even")
+else:
+    print("The given number is Odd")
